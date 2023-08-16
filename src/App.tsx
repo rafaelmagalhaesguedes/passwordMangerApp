e26c99ebd8e87d1b4f7a7503276865dfcee5e5a7
Rafael <rafaelmagalhaes1@live.com>
2023-08-16 09:02:37 -0300

    feat: requisitos 4 e 5 ok

--- src/App.tsx
+++ src/App.tsx
@@ -4,24 +4,19 @@ import Header from './components/Header';
 import Form from './components/Form';
 
 function App(): ReactElement {
-  // Estado para controlar se o formulário deve ser exibido ou não
   const [showForm, setShowForm] = useState(false);
 
-  // Manipulador do evento de click para mostrar formulário
   const handleShowForm = () => {
     setShowForm(true);
   };
 
   return (
-    <div>
-      {/* Componente Header */}
+    <div className="container">
       <Header />
 
       {showForm ? (
-        // Renderiza o componente formulário se showForm for true
         <Form />
       ) : (
-        // Renderiza o botão se showForm for false
         <button id="button" onClick={ handleShowForm }> Cadastrar nova senha </button>
       )}
     </div>
--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,48 +1,92 @@
-import { ReactElement, useState } from 'react';
+import { ReactElement, useState, ChangeEvent, useEffect } from 'react';
+
+interface FormData {
+  serviceName: string;
+  login: string;
+  password: string;
+}
 
 function Form(): ReactElement {
   const [showForm, setShowForm] = useState(true);
-  const [password, setPassword] = useState('');
+  const [formData, setFormData] = useState<FormData>({
+    serviceName: '',
+    login: '',
+    password: '',
+  });
   const [passwordError, setPasswordError] = useState('');
+  const [isSubmitDisabled, setIsSubmitDisabled] = useState(true);
+  const [isLengthValid, setIsLengthValid] = useState(false);
+  const [isLengthWithinRange, setIsLengthWithinRange] = useState(false);
+  const [hasLettersAndNumbers, setHasLettersAndNumbers] = useState(false);
+  const [hasSpecialCharacter, setHasSpecialCharacter] = useState(false);
+  const valid = 'valid-password-check';
+  const invalid = 'invalid-password-check';
+
+  const handleServiceNameChange = (event: ChangeEvent<HTMLInputElement>) => {
+    const nameValue = event.target.value;
+    setFormData((prevData) => ({ ...prevData, serviceName: nameValue }));
+  };
+
+  const handleLoginChange = (event: ChangeEvent<HTMLInputElement>) => {
+    const loginValue = event.target.value;
+    setFormData((prevData) => ({ ...prevData, login: loginValue }));
+  };
+
+  const handlePasswordChange = (event: ChangeEvent<HTMLInputElement>) => {
+    const newPassword = event.target.value;
+    setFormData((prevData) => ({ ...prevData, password: newPassword }));
+    setPasswordError(validatePassword(newPassword) ? ''
+      : 'A senha não atende aos requisitos.');
+  };
+
+  const validatePassword = (pass: string) => {
+    const passwordRegex = /^(?=.*[a-zA-Z])(?=.*\d)(?=.*[!@#$%^&*()_+{}[\]:;<>,.?~\\/-]).{8,16}$/;
+    return passwordRegex.test(pass);
+  };
+
+  useEffect(() => {
+    setIsLengthValid(formData.password.length >= 8);
+    setIsLengthWithinRange(formData.password.length <= 16);
+    setHasLettersAndNumbers(/^(?=.*[a-zA-Z])(?=.*\d).+$/.test(formData.password));
+    setHasSpecialCharacter(/[!@#$%^&*()_+{}[\]:;<>,.?~\\/-]/.test(formData.password));
+
+    const isServiceNameValid = formData.serviceName.trim() !== '';
+    const isLoginValid = formData.login.trim() !== '';
+    setIsSubmitDisabled(!(isServiceNameValid && isLoginValid && isLengthValid
+      && isLengthWithinRange && hasLettersAndNumbers && hasSpecialCharacter));
+  }, [formData, isLengthValid, isLengthWithinRange,
+    hasLettersAndNumbers, hasSpecialCharacter]);
 
   const handleCancel = () => {
     setShowForm(false);
   };
 
-  const handlePasswordChange = (event: any) => {
-    const newPassword = event.target.value;
-    setPassword(newPassword);
-    setPasswordError(validatePassword(newPassword) ? ''
-      : 'A senha não atende aos requisitos.');
-  };
-
-  const validatePassword = (pass: any) => {
-    const minLength = 8;
-    const maxLength = 16;
-
-    const hasValidLength = pass.length >= minLength && pass.length <= maxLength;
-    const hasLettersAndNumbers = /^(?=.*[a-zA-Z])(?=.*\d).+$/.test(pass);
-    const hasSpecialCharacter = /[!@#$%^&*()_+{}[\]:;<>,.?~\\/-]/.test(pass);
-
-    return hasValidLength && hasLettersAndNumbers && hasSpecialCharacter;
-  };
-
   return (
     <div>
       {showForm ? (
-
         <form>
           <label htmlFor="inputService">Nome do serviço</label>
-          <input type="text" id="inputService" required />
+          <input
+            type="text"
+            id="inputService"
+            value={ formData.serviceName }
+            onChange={ handleServiceNameChange }
+          />
 
           <label htmlFor="inputLogin">Login</label>
-          <input type="text" id="inputLogin" required />
+          <input
+            type="text"
+            id="inputLogin"
+            value={ formData.login }
+            onChange={ handleLoginChange }
+            required
+          />
 
           <label htmlFor="inputPass">Senha</label>
           <input
             type="password"
             id="inputPass"
-            value={ password }
+            value={ formData.password }
             onChange={ handlePasswordChange }
             required
           />
@@ -51,8 +95,23 @@ function Form(): ReactElement {
           <label htmlFor="inputUrl">URL</label>
           <input type="text" id="inputUrl" />
 
-          <button id="register">Cadastrar</button>
-          <button id="cancel" onClick={ handleCancel }>Cancelar</button>
+          <div className={ `password-check ${isLengthValid ? valid : invalid}` }>
+            Possuir 8 ou mais caracteres
+          </div>
+          <div className={ `password-check ${isLengthWithinRange ? valid : invalid}` }>
+            Possuir até 16 caracteres
+          </div>
+          <div className={ `password-check ${hasLettersAndNumbers ? valid : invalid}` }>
+            Possuir letras e números
+          </div>
+          <div className={ `password-check ${hasSpecialCharacter ? valid : invalid}` }>
+            Possuir algum caractere especial
+          </div>
+
+          <button id="register" disabled={ isSubmitDisabled }>Cadastrar</button>
+          <button id="cancel" onClick={ handleCancel }>
+            Cancelar
+          </button>
         </form>
       ) : (
         <button id="button">Cadastrar nova senha</button>
