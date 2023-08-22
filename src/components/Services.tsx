6f74e18d99c0fd898bdc9a644091b1b9514c2f0b
Rafael <rafaelmagalhaes1@live.com>
2023-08-22 12:32:37 -0300

    style: initial page styled

--- src/components/Services.tsx
+++ src/components/Services.tsx
@@ -1,5 +1,7 @@
 import ServiceList from './Services/ServiceList';
 import { ServiceData } from '../types/types';
+import Locker from '../images/locker-img.svg';
+import Vetor from '../images/Vector 1.png';
 
 type ServicesProps = {
   services: ServiceData[];
@@ -15,7 +17,11 @@ function Services({ services, onRemoveService }: ServicesProps) {
           onRemoveService={ onRemoveService }
         />
       ) : (
-        <p>Nenhuma senha cadastrada</p>
+        <div className="message-empty-services">
+          <img className="vector" src={ Vetor } alt="Vetor" />
+          <p className="message">Não há nenhuma senha cadastrada...</p>
+          <img className="locker" src={ Locker } alt="Locker" />
+        </div>
       )}
     </div>
   );
